b24da12df9a3a87baaaaf1110370bb05ecf29b55
Sérgio Prates dos Santos <sergio.prates@lambda3.com.br>
2022-12-16 20:37:23 -0300

    wip

--- src/KindxtApp/Extensions/ServiceCollectionExtensions.cs
+++ src/KindxtApp/Extensions/ServiceCollectionExtensions.cs
@@ -5,25 +5,25 @@ using Microsoft.Extensions.DependencyInjection;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 
-namespace Kindxt.Extensions
+namespace Kindxt.Extensions;
+public static class ServiceCollectionExtensions
 {
-    public static class ServiceCollectionExtensions
+    public static IServiceCollection RegisterDependencies(this IServiceCollection services)
     {
-        public static IServiceCollection RegisterDependencies(this IServiceCollection services)
-        {
-            services.AddSingleton<ISerializer>(new SerializerBuilder()
-            .WithNamingConvention(CamelCaseNamingConvention.Instance)
-            .Build());
+        services.AddSingleton<ISerializer>(new SerializerBuilder()
+        .WithNamingConvention(CamelCaseNamingConvention.Instance)
+        .Build());
 
-            services.AddSingleton<IDeserializer>(new DeserializerBuilder()
-            .WithNamingConvention(CamelCaseNamingConvention.Instance)
-            .Build());
+        services.AddSingleton<IDeserializer>(new DeserializerBuilder()
+        .WithNamingConvention(CamelCaseNamingConvention.Instance)
+        .Build());
 
-            services.AddSingleton<KindClusterBuilder>();
-            services.AddSingleton<FileManager>();
-            services.AddSingleton<KindProcess>();
+        services.AddSingleton<KindClusterBuilder>();
+        services.AddSingleton<KindProcess>();
 
-            return services;
-        }
+        services.AddSingleton<FileManager>();
+        services.AddSingleton<HelmChartManager>();
+
+        return services;
     }
 }
